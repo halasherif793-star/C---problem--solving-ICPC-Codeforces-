189852ba7117385de6c0478cf35b229851d964df
halasherif793-star <halasherif793@gmail.com>
2026-08-05 16:05:14 +0300

    Add files via upload

---
+++ C++ problem solving (ICPC Codeforces)/problem A/problem B/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+       int number1;
+  long long number2;
+  char c1;
+  float number3;
+  double number4;
+  cin>>number1>>number2>>c1>>number3>>number4;
+  cout<< number1<<endl;
+  cout<< number2<<endl;
+  cout<< c1<<endl;
+  cout<<number3<<endl;
+  cout<<number4<<endl;
+
+
+
+
+
+
+    return 0;
+}
